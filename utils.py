eae1e4a9c3f653c296e2f6f9f51a0b2d024f052d
20Julia02 <20juliabialas02@gmail.com>
2025-11-27 22:54:31 +0100

    Usunieto opisy

--- utils.py
+++ utils.py
@@ -1,15 +1,6 @@
 import arcpy
 
 def get_path(end_id, prev):
-    """
-    Rekonstruuje ścieżkę, gdzie:
-    prev[node_id] = Edge prowadząca do node_id
-
-    Zwraca:
-        total_length : suma długości wszystkich krawędzi
-        path_nodes   : lista kolejnych node_id
-        path_edges   : lista obiektów Edge w kolejności
-    """
     path_nodes = [end_id]
     path_edges = []
     current = end_id
@@ -27,16 +18,6 @@ def get_path(end_id, prev):
     return total_length, path_nodes, [e.id for e in path_edges]
 
 def get_nearest_vertex_id(input_point, target_layer):
-    """
-    Znajduje wartość atrybutu vertex_id najbliższego wierzchołka 
-    na warstwie target_layer względem podanego punktu (input_point). 
-    Punkt musi leżeć w promieniu 1 km od najbliższego punktu na warstwie
-    Args:
-        input_point (PointGeometry): punkt wejściowy
-        target_layer (str): nazwa lub ścieżka do warstwy wierzchołków
-    Returns:
-        int: vertex_id najbliższego wierzchołka
-    """
     arcpy.analysis.Near(input_point, target_layer, "1000 METERS")
     with arcpy.da.SearchCursor(input_point, ["NEAR_FID"]) as cursor:
         near_fid = next(cursor)[0]
@@ -49,7 +30,6 @@ def get_nearest_vertex_id(input_point, target_layer):
         return None
 
 def format_time(minutes):
-    """Zwraca czas w formacie H M S na podstawie liczby minut."""
     total_seconds = int(minutes * 60)
     hours = total_seconds // 3600
     minutes = (total_seconds % 3600) // 60
@@ -66,7 +46,6 @@ def format_time(minutes):
 
     
 def orient_sequence(edge1, edge2):
-    """Zwraca listę [first, mid, last], lub None jeśli krawędzie nie są połączone."""
     (u1, v1) = edge1
     (u2, v2) = edge2
     common = {u1, v1} & {u2, v2}
